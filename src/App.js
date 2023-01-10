32cefca00f8a3a36345e92192ab7564485631579
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-10 11:59:56 -0500

    Added Banner and homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Banner />
     </div>
   );
 }
